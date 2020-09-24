bd2a2efdfe899daf1b7af3643e149ce1ba116a73
epic <mpaudel100@gmail.com>
2020-09-24 16:02:16 -0700

    c33

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+var database, position;
+
 var ball;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    database = firebase.database();
+
+    var ballRef = database.ref("ball/position");
+
+    ballRef.on("value", readPos, showError); 
 }
 
 function draw(){
@@ -23,7 +31,32 @@ function draw(){
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function changePosition(x, y){
+    database.ref("ball/position").set({
+        x: ball.x + x,
+        y: ball.y + y
+    })
 }
+
+function readPos(data) {
+    position = data.val();
+
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError() {
+    console.log("error");
+}
+
+
+
+
+
+
+
+
+
+
+
+
